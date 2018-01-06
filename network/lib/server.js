111df2fb7bdd22784da1cd15382a73490990256e
Fuling <mon.samuel@qq.com>
2018-01-06 11:14:29 +0800

    init: add grpc and linvodb3 moules
        create a network folder, functions about peer to peer will write here. linvodb3 is a good module for leveldb

---
+++ network/lib/server.js
@@ -0,0 +1,47 @@
+const rpc = require('grpc')
+const fs = require('fs')
+const path = require('path')
+
+class Server {
+    constructor(ip, port) {
+        Object.assign(this, { ip, port })
+        this.services = {}
+        this.functions = {}
+    }
+
+    autoRun(protoDir) {
+        fs.readdir(protoDir, (err, files) => {
+            if(err) {
+                return console.error(err)
+            }
+            files.forEach((file) => {
+                const filePart = path.parse(file)
+                const serviceName = filePart.name
+                const packageName = filePart.name
+                const extName = filePart.ext
+                const filePath = path.join(protoDir, file)
+                if (extName === '.js') {
+                    const functions = require(filePath).default
+                    this.functions[serviceName] = Object.assign({}, functions)
+                } else if (extName === '.proto') {
+                    this.services[serviceName] = rpc.load(filePath)[packageName][serviceName].service
+                }
+            }, files)
+            return this.runServer()
+        })
+    }
+
+    runServer() {
+        const server = new rpc.Server()
+
+        R.forEach((serviceName) => {
+            const service = this.services[serviceName]
+            server.addService(service, this.functions[serviceName])
+        }, R.keys(this.services))
+
+        server.bind(`${this.ip}:${this.port}`, rpc.ServerCredentials.createInsecure())
+        server.start()
+    }
+}
+ 
+exports.default = Server;
