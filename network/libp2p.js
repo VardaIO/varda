fd4ddbdc2acd7f785e1d3b53a6be0ba66891a517
Fuling <mon.samuel@qq.com>
2018-01-16 21:03:36 +0800

    feat: a /addrs protocol
    this protocol can add peers from others peers, I am tesing it now

--- network/libp2p.js
+++ network/libp2p.js
@@ -4,10 +4,16 @@ const peerId = require('peer-id')
 const pify = require('pify')
 const rootPath = require('app-root-path')
 const multiaddr = require('multiaddr')
+const { values } = require('lodash')
+const pb = require('protocol-buffers')
+const fs = require('fs')
 const VARDA_HOME = process.env.VARDA_HOME || os.homedir() + '/.varda'
 const privateKey = require(VARDA_HOME + '/keys.json').PrivateKey
 const config = require(`${rootPath}/config.json`)
 const Node = require('./node-bundle')
+
+const msg = pb(fs.readFileSync('./protos/node.proto'))
+
 
 const createNode = () => {
     return pify(peerId.createFromPrivKey)(privateKey) // peerid 
@@ -22,13 +28,37 @@ const createNode = () => {
         })
         .then(peerInfo => { return new Node(peerInfo, config) })
 }
-
+function getPeers(node) {
+    let peers = []
+    values(node.peerBook.getAll()).forEach((peer) => {
+        const addr = peer.isConnected()
+        if (!addr) { return }
+        peers.push(addr.toString())
+    })
+    const buf = msg.addrs.encode({
+        addrs: peers
+    })
+    // console.log(msg.addrs.decode(buf))
+    return buf
+}
 setImmediate(async () => {
     let node = await createNode()
     node.start(() => {
         console.log('node has started (true/false):', node.isStarted())
         console.log('listening on:')
         node.peerInfo.multiaddrs.forEach((ma) => console.log(ma.toString()))
+        
+        node.handle('/addrs', (protocol, conn) => {
+            pull(
+                conn,
+                pull.map((v)=>msg.addrs.decode(v)),
+                pull.collect(function (err, array) {
+                          console.log(array)
+                        })
+            )
+
+        })
+
         node.on('peer:discovery', (peerInfo) => {
             console.log('Discovered a peer')
             const idStr = peerInfo.id.toB58String()
@@ -41,13 +71,28 @@ setImmediate(async () => {
         node.on('peer:connect', (peerInfo) => {
             console.log('connected a peer')
             const idStr = peerInfo.id.toB58String()
+            node.dial(peerInfo, '/addrs', (err, conn) => {
+                if (err) console.log(err)
+                pull(
+                    pull.values([getPeers(node)]),
+                    conn
+                  )
+            })
+            
             console.log('connected: ' + idStr)
             console.log(node.peerBook)
         })
-        
+
         node.on('peer:disconnect', (peerInfo) => {
             console.log('disconnect a peer')
-            console.log(node.peerBook)
+            // console.log(node.peerBook)
         })
+
+
+
+        setInterval(() => {
+            console.log(geetPeers(node))
+        }, 1000 * 10)
     })
+
 })
