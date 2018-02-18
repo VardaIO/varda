96f89fd21836c9a0f5460772ffbc6af99ed3bb2b
Fuling <mon.samuel@qq.com>
2018-02-18 17:22:47 +0800

    feat: if a peer have a public ip, then sen it to others

--- network/p2p.js
+++ network/p2p.js
@@ -23,17 +23,25 @@ const bootstrap = require('./bootstrap')
 config.bootstrap = bootstrap
 const msg = pb(fs.readFileSync('./protos/node.proto'))
 
-const createNode = () => {
+/**
+ * todo:
+ * if local peer have a public ip, then broadcast this ip and peer id to the world
+ * the net should have a public ip list, every 1 min send it to other peers 
+ */
+
+const createNode = async () => {
     return pify(peerId.createFromPrivKey)(privateKey) // peerid 
         .then(id => {
             return new PeerInfo(id)
         }) // peerInfo
         .then(peerInfo => {
-            if (config.signal) {
-                let ma = multiaddr(config.signal)
-                peerInfo.multiaddrs.add(ma)
+            let publicIP = await getPublicIp()
+            if (!publicIP) {
+                if (config.signal) {
+                    let ma = multiaddr(config.signal)
+                    peerInfo.multiaddrs.add(ma)
+                }
             }
-
             let addr = `/ip4/0.0.0.0/tcp/${config.Port}`
             let ma = multiaddr(addr)
             peerInfo.multiaddrs.add(ma)
@@ -45,7 +53,29 @@ const createNode = () => {
         })
 }
 
-setImmediate(async () => {
+const getPublicIp = async () => {
+    try {
+        return await publicIP.v4()
+    } catch (error) {
+        return null
+    }
+}
+
+const encodePublicIps = (publicIps) => {
+    const buf = msg.addrs.encode({
+        addrs: publicIps
+    })
+    return buf
+}
+// msg.addrs.decode(v)
+// let encode = encodePublicIps(["hahahah:1111","lalalal:9991"])
+
+// console.log(encode)
+// console.log()
+// return
+const runP2p = async () => {
+
+    let publicIpsList = []
 
     let node = await createNode()
 
@@ -53,15 +83,6 @@ setImmediate(async () => {
         console.log('node has started (true/false):', node.isStarted())
         console.log('listening on:')
         node.peerInfo.multiaddrs.forEach((ma) => console.log(ma.toString()))
-    })
-
-
-    node.handle('/t', (protocol, conn) => {
-        pull(
-            conn,
-            pull.map((s) => s.toString()),
-            pull.log()
-        )
     })
 
     // when discovery a peer, try to dial to this peer,if it can reply, 
@@ -82,6 +103,47 @@ setImmediate(async () => {
         console.log(colors.gray('Disconnect:'), peerInfo.id.toB58String())
     })
 
+    let publicIp = await getPublicIp()
+
+    node.handle('/getPubIp', (protocol, conn) => {
+        pull(
+            conn,
+            pull.map((ip) => msg.addr.decode(ip)),
+            pull.collect((err, array) => {
+                if (err) console.log(err)
+                if (publicIpsList.indexOf(array[0].addr) == -1) publicIpsList.push(array[0].addr)
+            })
+        )
+    })
+
+    if (publicIp) {
+        console.log(publicIp)
+        let id = node.peerInfo.id.toB58String()
+        let addr = `/ip4/${publicIP}/tcp/${config.Port}/ipfs/${id}`
+        const buf = msg.addr.encode({
+            addr: addr
+        })
+        setInterval(() => {
+            values(node.peerBook.getAll()).forEach((peer) => {
+                node.dial(peer, '/getPubIp', (err, conn) => {
+                    if (err) console.log(err)
+                    pull(
+                        pull.values([buf]),
+                        conn
+                    )
+                })
+            })
+        }, 1000 * 60)
+    }
+
+    node.handle('/t', (protocol, conn) => {
+        pull(
+            conn,
+            pull.map((s) => s.toString()),
+            pull.log()
+        )
+    })
+
     setInterval(() => {
         // let addrs = await encodePeers(node)
         // console.log(addrs)
@@ -98,4 +160,6 @@ setImmediate(async () => {
         })
     }, 2000)
 
-})
+}
+
+runP2p()
