74fdab38faaf38aa60c95e3863ee05d8db2d01dc
Fuling <mon.samuel@qq.com>
2018-02-20 23:27:40 +0800

    feat: if star from local, need not validate it and can put it in waiting pool directly

--- components/commission.js
+++ components/commission.js
@@ -6,6 +6,11 @@
  * 5. a star should have at least one on star'main chain index - 1
  * if a star confirmed with 2/3 , broadcast this star to the network at once
  */
+
+ /* todao 
+ * waiting pool 缺少一个内存清理工具
+ * 当大于一时间时，清理掉waiting pool中broadcast = true的star
+ */
 const pool = require('../database/pool')
 const Vailidate = require('./vailidate')
 const _ = require('lodash')
@@ -28,15 +33,23 @@ class Commission {
                 if (!property || !value) {
                     return
                 }
+                // if from local
+                if (value['starFrom'] && value['starFrom'] == 'local') {
+                    receiver[property] = value
+                    receiver[property].count = 0
+                    return
+                }
+
                 if (!new Vailidate().vailidateStarWithoutTransaction(value)) {
                     return
                 }
                 //1. 判断key（star hash）是否存在
                 const existKey = _.has(receiver, property)
-                //1.1存在：查看key中的count，若大于3/1则commit并广播，不大于则继续计数
+                //1.1存在：查看key中的count，若大于3/1则commit并广播(在receiver[property].broadcas不存在时)，不大于则继续计数
                 if (existKey) {
-                    if (receiver[property].count >= Math.floor(commissionNumber / 3) * 2) {
+                    if (receiver[property].count >= Math.floor(commissionNumber / 3) * 2 && !receiver[property].broadcast) {
                         //broadcast
+                        receiver[property].broadcast = true 
                         return
                     }
                     receiver[property].count++
@@ -73,7 +86,9 @@ setImmediate(async () => {
     console.log(b.waiting)
     const Star = require('./star')
     let star = await new Star().getStar('d3e07MIoj95eJDV29gX3Ydyi6MkZI23MsWFuAsEk0XQ=')
+    star.starFrom = 'local'
     b.waiting[star.star_hash] = star
+    console.log(b.waiting)
 
 })
 // setImmediate(async () => {
