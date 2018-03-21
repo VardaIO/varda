d224a2537790d78c73535d304b6dea180073a8d6
fuling <mon.samuel@qq.com>
2018-03-22 00:38:58 +0800

    fix: fix the starHash is gensis's

--- network/http/routes.js
+++ network/http/routes.js
@@ -53,6 +53,13 @@ router.post('/getBalance', async ctx => {
 
 router.post('/getStar', async ctx => {
   const starHash = ctx.request.body.starHash
+  const genesis = new Star().getGenesis()
+  
+  if (_.isEqual(starHash, genesis.star_hash)) {
+    ctx.body = { star: genesis }
+    return
+  }
+
   const star = await new Star().getStar(starHash)
   ctx.body = { star }
 })
