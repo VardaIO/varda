29136cb710cb38044b43c70d473f08a729a2d702
fuling <mon.samuel@qq.com>
2018-03-28 20:13:03 +0800

    feat: add mnemonic in config

--- index.js
+++ index.js
@@ -26,59 +26,62 @@ const start = async () => {
 
     const hd = new HD()
 
-    const askForMnemonic = () => {
-      const questions = [
-        {
-          name: 'mnemonic',
-          type: 'password',
-          message: 'Enter your wallet mnemonic:',
-          validate: function(value) {
-            if (value.length) {
-              if (hd.validateMnemonic(value)) {
-                return true
-              }
-              return 'mnemonic is wrong'
-            } else {
-              console.log(
-                `\n Your mnemonic is here, please remember to backup it. \n ${hd.genMnemonic()}`
-              )
-              console.log(
-                '\n mnemonic has been generated, the Varda will exit, input mnemonic next time'
-              )
-              return process.exit()
-            }
-          }
-        },
-        {
-          name: 'password',
-          type: 'password',
-          message: 'Enter your mnemonic password (option):'
-        }
-      ]
-
-      if (config.commission) {
-        return inquirer.prompt(questions)
-      }
-
-      return Promise.resolve('welcome to use Varda')
-    }
-
-    const answer = await askForMnemonic()
-    // console.log(answer)
     let seed
     let sk
-    // const Utils = require('./components/utils')
-    // console.log(answer.password)
-    // console.log(hd.getSeed(answer.mnemonic, answer.password))
-    if (typeof answer === 'object') {
-      if (answer.password) {
-        seed = hd.getSeed(answer.mnemonic, answer.password)
-        // console.log('have password')
-        sk = hd.genKeypair(0, seed).secretKey
-      } else {
-        seed = hd.getSeed(answer.mnemonic)
 
-        sk = hd.genKeypair(0, seed).secretKey
+    if (config.mnemonic !== '') {
+      seed = hd.getSeed(config.mnemonic)
+      sk = hd.genKeypair(0, seed).secretKey
+    } else {
+      const askForMnemonic = () => {
+        const questions = [
+          {
+            name: 'mnemonic',
+            type: 'password',
+            message: 'Enter your wallet mnemonic:',
+            validate: function(value) {
+              if (value.length) {
+                if (hd.validateMnemonic(value)) {
+                  return true
+                }
+                return 'mnemonic is wrong'
+              } else {
+                console.log(
+                  `\n Your mnemonic is here, please remember to backup it. \n ${hd.genMnemonic()}`
+                )
+                console.log(
+                  '\n mnemonic has been generated, the Varda will exit, input mnemonic next time'
+                )
+                return process.exit()
+              }
+            }
+          },
+          {
+            name: 'password',
+            type: 'password',
+            message: 'Enter your mnemonic password (option):'
+          }
+        ]
+
+        if (config.commission) {
+          return inquirer.prompt(questions)
+        }
+
+        return Promise.resolve('welcome to use Varda')
+      }
+
+      const answer = await askForMnemonic()
+
+      if (typeof answer === 'object') {
+        if (answer.password) {
+          seed = hd.getSeed(answer.mnemonic, answer.password)
+          // console.log('have password')
+          sk = hd.genKeypair(0, seed).secretKey
+        } else {
+          seed = hd.getSeed(answer.mnemonic)
+
+          sk = hd.genKeypair(0, seed).secretKey
+        }
       }
     }
     // console.log(new Utils().getAddressFromSk(sk))
