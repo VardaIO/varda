50d59280cf863faf008b979bd39f0b2573827e13
fuling <mon.samuel@qq.com>
2018-04-03 16:06:07 +0800

    feat: add _dealMessage function

--- components/Dapps/container.js
+++ components/Dapps/container.js
@@ -1,7 +1,9 @@
+import { resolve } from 'path'
+
 // const cp = require('child_process');
 const spawn = require('child_process').spawn
 
-const Star = require('./api/stars')
+// const Star = require('./api/stars')
 
 /**
  * star,
@@ -14,7 +16,40 @@ const Star = require('./api/stars')
 
 class Container {
   constructor(path) {
-      this.path = path
+    this.path = path
+    this.apis = ['star', 'account', 'broadcast', 'sync', 'commission', 'db', 'model', 'utils']
+    this.api = {
+      star: require('./api/stars'),
+      account: require('./api/account'),
+      broadcast: require('./api/broadcast'),
+      sync: require('./api/sync'),
+      commission: require('./api/commission'),
+      db: require('./api/db'),
+      model: require('./api/model'),
+      utils: require('./api/utils')
+    }
+  }
+
+  _dealMessage(message) {
+    return new Promise((resolve, reject) => {
+      const apiName = message.api
+      const methodName = message.method
+      const args = message.args
+
+      if (
+        apiName === undefined ||
+        methodName === undefined ||
+        args === undefined
+      ) {
+        reject('message is wrong')
+      }
+
+      if(this.apis.indexOf(apiName) === -1) {
+        reject(`the is no api named ${apiName}`)
+      }
+
+      // todo: check method
+    })
   }
 
   newContainer() {
@@ -25,9 +60,9 @@ class Container {
     container.on('message', message => {
       /**
        * message style:
-       * 
+       *
        * {
-       *  module: <module name>,
+       *  api: <api name>,
        *  method: <method name>,
        *  args: <method arg>
        * }
@@ -40,4 +75,5 @@ class Container {
 }
 
 let c = new Container(`${require('app-root-path')}/Dapps/test`)
-c.newContainer()
+// c.newContainer()
+c._parseMessage({})
