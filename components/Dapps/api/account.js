69ce3806d8d89f6912b122cf612cff7e6989d7e3
fuling <mon.samuel@qq.com>
2018-04-06 00:13:38 +0800

    refactor: use async/ await to make it better

---
+++ components/Dapps/api/account.js
@@ -0,0 +1,31 @@
+const Utils = require('../../utils')
+const Wallet = require('../../hd-wallet')
+const accountFromComponent = require('../../account')
+
+class Account extends accountFromComponent {
+  newAccount() {
+    const utils = new Utils()
+    const wallet = new Wallet()
+    const mnemonic = wallet.genMnemonic()
+    const seed = wallet.getSeed(mnemonic)
+    const keypair = wallet.genKeypair(0, seed)
+    const sk = keypair.secretKey
+    const pk = keypair.publicKey
+    const address = utils.genAddress(pk)
+
+    return { mnemonic, address, sk, pk }
+  }
+}
+
+const a = new Account()
+
+let ac = a.newAccount()
+
+console.log(ac.address)
+setImmediate(async () => {
+  setInterval(async () => {
+    let b = await a.getBalance('VLRAJEAFXJBVYZQYT67YUQ3KJV53A')
+    console.log(`balance: ${b}`)
+    console.log(await a.getPk('VLRAJEAFXJBVYZQYT67YUQ3KJV53A'))
+  }, 1000)
+})
--- components/account.js
+++ components/account.js
@@ -5,68 +5,85 @@ class Account {
     this.address = address
   }
 
-  getBalance() {
-    return pool()
-      .acquire()
-      .then(client => {
-        try {
-          if (this.address == null || this.address == undefined) {
-            return
-          }
-          const income = client
-            .prepare(
-              `SELECT SUM(amount) AS receive FROM transactions WHERE recpient='${
-                this.address
-              }'`
-            )
-            .get().receive
+  async getBalance(address = null, dbFilePath = null) {
+    let client =
+      dbFilePath === null
+        ? await pool().acquire()
+        : await pool(dbFilePath).acquire()
 
-          if (income === null) {
-            return 0
-          }
+    try {
+      if (
+        this.address === null ||
+        (this.address === undefined && address === null)
+      ) {
+        return
+      }
 
-          const payment = client
-            .prepare(
-              `SELECT SUM(amount) AS pay FROM transactions WHERE sender='${
-                this.address
-              }'`
-            )
-            .get().pay
-          const balance = income - payment
-          return balance
-        } catch (error) {
-          return Promise.reject(error)
-        } finally {
-          pool().release(client)
-        }
-      })
+      if (address === null) {
+        address = this.address
+      }
+
+      const income = client
+        .prepare(
+          `SELECT SUM(amount) AS receive FROM transactions WHERE recpient='${address}'`
+        )
+        .get().receive
+
+      if (income === null) {
+        return 0
+      }
+
+      const payment = client
+        .prepare(
+          `SELECT SUM(amount) AS pay FROM transactions WHERE sender='${address}'`
+        )
+        .get().pay
+      const balance = income - payment
+      return Promise.resolve(balance)
+    } catch (error) {
+      return Promise.reject(error)
+    } finally {
+      const loan = new Map().get(client)
+      if (loan !== undefined) {
+        pool().release(client)
+      }
+    }
   }
 
-  getPk() {
-    return pool()
-      .acquire()
-      .then(client => {
-        try {
-          if (this.address == null || this.address == undefined) {
-            return
-          }
+  async getPk(address = null, dbFilePath = null) {
+    let client =
+      dbFilePath === null
+        ? await pool().acquire()
+        : await pool(dbFilePath).acquire()
 
-          const pk = client
-            .prepare(
-              `SELECT pk FROM account_pks WHERE address='${this.address}'`
-            )
-            .get()
+    try {
+      if (
+        this.address === null ||
+        (this.address === undefined && address === null)
+      ) {
+        return
+      }
 
-          if (pk == undefined) {
-            return null
-          }
-          return pk.pk
-        } catch (error) {
-          return Promise.reject(error)
-        } finally {
-          pool().release(client)
-        }
-      })
+      if (address === null) {
+        address = this.address
+      }
+
+      const pk = client
+        .prepare(`SELECT pk FROM account_pks WHERE address='${address}'`)
+        .get()
+
+      if (pk == undefined) {
+        return Promise.resolve(null)
+      }
+      return Promise.resolve(pk.pk)
+    } catch (error) {
+      return Promise.reject(error)
+    } finally {
+      const loan = new Map().get(client)
+      if (loan !== undefined) {
+        pool().release(client)
+      }
+    }
   }
 
   async checkTransaction(amount) {
