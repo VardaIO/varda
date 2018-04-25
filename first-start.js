a6722838d6e98826f070a5a6721727cb2681d15e
fuling <mon.samuel@qq.com>
2018-04-25 14:46:16 +0800

    refactor: separate initDb and addGeneisStar in initVarda

--- first-start.js
+++ first-start.js
@@ -13,6 +13,126 @@ const createVardaHome = async () => {
   }
 }
 
+const initDb = async pool => {
+  const client = await pool().acquire()
+  try {
+    client
+      .prepare(
+        `CREATE TABLE IF NOT EXISTS stars (
+            star CHAR(44) PRIMARY KEY NOT NULL,
+            main_chain_index BIGINT NOT NULL,
+            timestamp INT NOT NULL,
+            payload_hash CHAR(64) NOT NULL,
+            author_address CHAR(29) NOT NULL,
+            signature CHAR(128)
+            )`
+      )
+      .run()
+    client
+      .prepare('CREATE INDEX IF NOT EXISTS mci ON stars (main_chain_index)')
+      .run()
+
+    client
+      .prepare(
+        `CREATE TABLE IF NOT EXISTS  parenthoods (
+            child_star CHAR(44) NOT NULL,
+            parent_star CHAR(44) NOT NULL,
+            parent_index INT NOT NULL DEFAULT 0,
+        
+        PRIMARY KEY (
+            parent_star,
+            child_star
+        )
+        )`
+      )
+      .run()
+    client
+      .prepare(
+        'CREATE INDEX IF NOT EXISTS byChildStar ON parenthoods (child_star)'
+      )
+      .run()
+
+    client
+      .prepare(
+        `CREATE TABLE IF NOT EXISTS transactions (
+                star CHAR(44) PRIMARY KEY NOT NULL,
+                type INT NOT NULL,
+                sender CHAR(29) NOT NULL,
+                amount BIGINT NOT NULL,
+                recpient CHAR(29)
+        )`
+      )
+      .run()
+    client
+      .prepare('CREATE INDEX IF NOT EXISTS byStar ON transactions (star)')
+      .run()
+
+    client
+      .prepare(
+        `CREATE TABLE IF NOT EXISTS account_pks (
+               address  CHAR(29) PRIMARY KEY NOT NULL,
+               pk CHAR(64) NOT NULL
+            )`
+      )
+      .run()
+    client
+      .prepare('CREATE INDEX IF NOT EXISTS byAddress ON account_pks (address)')
+      .run()
+    return Promise.resolve()
+  } catch (error) {
+    return Promise.reject(error)
+  } finally {
+    const loan = new Map().get(client)
+    if (loan !== undefined) {
+      pool().release(client)
+    }
+  }
+}
+
+const addGenesisStar = async (pool, Star) => {
+  const client = await pool().acquire()
+  try {
+    if (
+      !client.prepare('SELECT star FROM stars WHERE main_chain_index=0').get()
+    ) {
+      const star = new Star()
+      const genesis = star.getGenesis()
+      let addGenesis = client.prepare(
+        'INSERT INTO stars VALUES (?, ?, ?, ?, ?, ?)'
+      )
+      addGenesis.run(
+        genesis.star_hash,
+        0,
+        genesis.timestamp,
+        genesis.payload_hash,
+        genesis.authorAddress,
+        'null'
+      )
+
+      let addGenesisTx = client.prepare(
+        'INSERT INTO transactions VALUES (@star, @type, @sender, @amount, @recpient)'
+      )
+      addGenesisTx.run({
+        star: genesis.star_hash,
+        type: 0,
+        sender: genesis.transaction.sender,
+        amount: genesis.transaction.amount,
+        recpient: genesis.transaction.recpient
+      })
+
+      console.log('added a genesis star ')
+      return Promise.resolve()
+    }
+  } catch (error) {
+    return Promise.reject(error)
+  } finally {
+    const loan = new Map().get(client)
+    if (loan !== undefined) {
+      pool().release(client)
+    }
+  }
+}
+
 const initVarda = () => {
   createVardaHome()
     .then(async () => {
@@ -26,120 +146,11 @@ const initVarda = () => {
         return Promise.reject(error)
       }
     })
-    .then(() => {
+    .then(async () => {
       const Star = require('./components/star')
       const pool = require('./database/pool')
-      pool()
-        .acquire()
-        .then(client => {
-          client
-            .prepare(
-              `CREATE TABLE IF NOT EXISTS stars (
-            star CHAR(44) PRIMARY KEY NOT NULL,
-            main_chain_index BIGINT NOT NULL,
-            timestamp INT NOT NULL,
-            payload_hash CHAR(64) NOT NULL,
-            author_address CHAR(29) NOT NULL,
-            signature CHAR(128)
-            )`
-            )
-            .run()
-          client
-            .prepare(
-              'CREATE INDEX IF NOT EXISTS mci ON stars (main_chain_index)'
-            )
-            .run()
-
-          client
-            .prepare(
-              `CREATE TABLE IF NOT EXISTS  parenthoods (
-            child_star CHAR(44) NOT NULL,
-            parent_star CHAR(44) NOT NULL,
-            parent_index INT NOT NULL DEFAULT 0,
-        
-        PRIMARY KEY (
-            parent_star,
-            child_star
-        )
-        )`
-            )
-            .run()
-          client
-            .prepare(
-              'CREATE INDEX IF NOT EXISTS byChildStar ON parenthoods (child_star)'
-            )
-            .run()
-
-          client
-            .prepare(
-              `CREATE TABLE IF NOT EXISTS transactions (
-                star CHAR(44) PRIMARY KEY NOT NULL,
-                type INT NOT NULL,
-                sender CHAR(29) NOT NULL,
-                amount BIGINT NOT NULL,
-                recpient CHAR(29)
-        )`
-            )
-            .run()
-          client
-            .prepare('CREATE INDEX IF NOT EXISTS byStar ON transactions (star)')
-            .run()
-
-          client
-            .prepare(
-              `CREATE TABLE IF NOT EXISTS account_pks (
-               address  CHAR(29) PRIMARY KEY NOT NULL,
-               pk CHAR(64) NOT NULL
-            )`
-            )
-            .run()
-          client
-            .prepare(
-              'CREATE INDEX IF NOT EXISTS byAddress ON account_pks (address)'
-            )
-            .run()
-
-          if (
-            !client
-              .prepare('SELECT star FROM stars WHERE main_chain_index=0')
-              .get()
-          ) {
-            const star = new Star()
-            const genesis = star.getGenesis()
-            let addGenesis = client.prepare(
-              'INSERT INTO stars VALUES (?, ?, ?, ?, ?, ?)'
-            )
-            addGenesis.run(
-              genesis.star_hash,
-              0,
-              genesis.timestamp,
-              genesis.payload_hash,
-              genesis.authorAddress,
-              'null'
-            )
-
-            let addGenesisTx = client.prepare(
-              'INSERT INTO transactions VALUES (@star, @type, @sender, @amount, @recpient)'
-            )
-            addGenesisTx.run({
-              star: genesis.star_hash,
-              type: 0,
-              sender: genesis.transaction.sender,
-              amount: genesis.transaction.amount,
-              recpient: genesis.transaction.recpient
-            })
-
-            console.log('added a genesis star ')
-          }
-          const loan = new Map().get(client)
-          if (loan !== undefined) {
-            pool().release(client)
-          }
-          return Promise.resolve()
-        })
-        .catch(error => {
-          return Promise.reject(error)
-        })
+      await initDb(pool)
+      await addGenesisStar(pool, Star)
     })
     .then(async () => {
       try {
